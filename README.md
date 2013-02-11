7d2f368ae9e6d766cec744c4bf0b8fb37e4ffd22
Robert <rwallhead@gmail.com>
2013-02-11 12:05:00 +1100

    Working, but very basic, script

--- README.md
+++ README.md
@@ -11,11 +11,24 @@ Obviously this'll be a Python project, using Python 2.7.3 on the Pi.
 Usage
 -----
 
-Would be nice to be able to call with a IO pin and a command to run eg:
+Would be nice to be able to call with a IO pin (1-26), a target state and a
+command to run eg:
 
-    ./raspir.py 11 logger Movement detected!
+    sudo python raspir.py 11 low logger Movement detected!
 
 Dependencies
 ------------
 
     sudo apt-get install python-rpi.gpio
+
+Electronics
+-----------
+
+Will connect to P1-02 (5V), P1-06 (GND) and P1-03 (GPIO00). I've chosen P1-03 as
+it has a built-in pull-up resistor to 3.3v so will work with the PIR sensor's
+open-collector output.
+
+Datasheets
+----------
+
+    PIR Sensor http://www.sparkfun.com/datasheets/Sensors/Proximity/SE-10.pdf
--- raspir.py
+++ raspir.py
@@ -1,1 +1,30 @@
+import os
 import RPi.GPIO
+import sys
+import time
+
+
+def monitor(pin, state, command):
+    RPi.GPIO.setmode(RPi.GPIO.BOARD)
+    RPi.GPIO.setup(pin, RPi.GPIO.IN)
+    while True:
+        input_value = RPi.GPIO.input(pin)
+        if input_value == state:
+            os.system(command)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    pin = command = state = None
+    try:
+        if len(sys.argv) < 4 or sys.argv[2].lower() not in ('low', 'high'):
+            raise Exception
+        pin = int(sys.argv[1])
+        state = sys.argv[2].lower() == 'high'
+        command = ' '.join(sys.argv[3:]).strip()
+    except:
+        print 'Usage: ./raspir.py [header pin (1-26)] [state (low/high)] [Command to run]'
+        sys.exit(1)
+
+    monitor(pin, state, command)
+
